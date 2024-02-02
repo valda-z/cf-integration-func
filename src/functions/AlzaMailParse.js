996cfc63e3a95cdabdb3ed756c7720f110e0e6d6
Valdemar Zavadsky <valda@cloudfield.cz>
2024-02-02 17:05:46 +0100

    download pdf

--- src/functions/AlzaMailParse.js
+++ src/functions/AlzaMailParse.js
@@ -13,22 +13,30 @@ app.http('AlzaMailParse', {
         const links = body.match(/<a href="(.*?)">.*?<\/a>/g).map(link => {
             const href = link.match(/<a href="(.*?)".*>/)[1];
             // only return the href if contains "https://www.alza.cz/Apps/pdfdoc.asp" or "ISDOCinvoicedownload"
-            if (href.includes("https://www.alza.cz/Apps/pdfdoc.asp") || href.includes("ISDOCinvoicedownload")){
+            if (href.includes("https://www.alza.cz/Apps/pdfdoc.asp") || href.includes("ISDOCinvoicedownload")) {
                 return href.replace(/&amp;/g, "&");
-            }            
+            }
         }).filter(link => link !== undefined);
         // map array to two objects for pdf and isdocs
         const docs = {
             pdf: links.find(link => link.includes("https://www.alza.cz/Apps/pdfdoc.asp")),
             isdoc: links.find(link => link.includes("ISDOCinvoicedownload"))
         };
+        // download pdf and isdoc
+        let pdf, isdoc;
+        if (docs.pdf) {
+            pdf = await fetch(docs.pdf);
+        }
+        if (docs.isdoc) {
+            isdoc = await fetch(docs.isdoc);
+        }
         // application/json
         return {
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                "docs": docs,
+                "pdf": pdf ? await pdf.arrayBuffer().then(buffer => Buffer.from(buffer).toString('base64')) : null,
             })
         };
     }
